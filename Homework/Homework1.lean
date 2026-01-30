03189753c715f982f02be24f48941cc0994a2016
yichuan-w <yichuan_wang@berkeley.edu>
2026-01-30 09:19:45 +0000

    finish test

--- Homework/Homework1.lean
+++ Homework/Homework1.lean
@@ -38,19 +38,19 @@ context. -/
 
 @[autogradedDef 1]
 def B : (α → β) → (γ → α) → γ → β :=
-  sorry
+  fun f g c ↦ f (g c)
 
 @[autogradedDef 1]
 def S : (α → β → γ) → (α → β) → α → γ :=
-  sorry
+  fun f g a ↦ f a (g a)
 
 @[autogradedDef 1]
 def moreNonsense : ((α → β) → γ → δ) → γ → β → δ :=
-  sorry
+  fun f c b ↦ f (fun _ ↦ b) c
 
 @[autogradedDef 1]
 def evenMoreNonsense : (α → β) → (α → γ) → α → β → γ :=
-  sorry
+  fun _ g a _ ↦ g a
 
 
 /- (2 points). Complete the following definition.
@@ -66,7 +66,7 @@ Note: Peirce is pronounced like the English word "purse". -/
 
 @[autogradedDef 2]
 def weakPeirce : ((((α → β) → α) → α) → β) → β :=
-  sorry
+  fun f ↦ f (fun g ↦ g (fun a ↦ f (fun _ ↦ a)))
 
 
 /- ## Question 2 (4 points): Snoc
@@ -77,13 +77,16 @@ end of a list. Your function should be defined by recursion and not using `++`
 
 @[autogradedDef 3]
 def snoc {α : Type} (xs: List α) (x : α) : List α :=
-  sorry
+  match xs with
+  | [] => [x]
+  | y :: ys => y :: snoc ys x
 
 /- (1 point). Convince yourself that your definition of `snoc` works by
 testing it on at least 2 more examples. -/
 
 #eval snoc [1] 2 -- expected: [1, 2]
--- invoke `#eval` here, include a comment showing the expected result
+#eval snoc [1, 2, 3] 4 -- expected: [1, 2, 3, 4]
+#eval snoc [] 5 -- expected: [5]
 
 
 /- ## Question 3 (6 points): Sum
@@ -92,8 +95,10 @@ testing it on at least 2 more examples. -/
 in a list. -/
 
 @[autogradedDef 3]
-def sum (xs: List ℕ):  ℕ :=
-  sorry
+def sum (xs: List ℕ): ℕ :=
+  match xs with
+  | [] => 0
+  | y :: ys => y + sum ys
 
 #eval sum [1, 12, 3]   -- expected: 16
 
@@ -106,7 +111,11 @@ def sum (xs: List ℕ):  ℕ :=
 
 Try to give meaningful names to your theorems. Use `sorry` as the proof. -/
 
--- enter your theorem statements here
+theorem sum_snoc (ms : List ℕ) (n : ℕ) : sum (snoc ms n) = n + sum ms := sorry
+
+theorem sum_append (ms ns : List ℕ) : sum (ms ++ ns) = sum ms + sum ns := sorry
+
+theorem sum_reverse (ns : List ℕ) : sum (ns.reverse) = sum ns := sorry
 
 /- ## Question 4 (4 points): Typing Derivation
 
@@ -116,6 +125,24 @@ draw horizontal bars) and `⊢` useful.
 
 Feel free to introduce abbreviations to avoid repeating large contexts `C`. -/
 
--- write your solution here
+/-
+Typing Derivation for B := fun f g c ↦ f (g c)
+
+Let C = f : α → β, g : γ → α, c : γ
+
+                ————————— Var         ————————— Var
+                C ⊢ g : γ → α         C ⊢ c : γ
+                ——————————————————————————————————— App
+————————————— Var         C ⊢ g c : α
+C ⊢ f : α → β
+——————————————————————————————————————————————————— App
+C ⊢ f (g c) : β
+———————————————————————————————————————————————————————————— Fun
+f : α → β, g : γ → α ⊢ (fun c : γ ↦ f (g c)) : γ → β
+——————————————————————————————————————————————————————————————————— Fun
+f : α → β ⊢ (fun g : γ → α ↦ fun c : γ ↦ f (g c)) : (γ → α) → γ → β
+——————————————————————————————————————————————————————————————————————————————— Fun
+⊢ (fun f : α → β ↦ fun g : γ → α ↦ fun c : γ ↦ f (g c)) : (α → β) → (γ → α) → γ → β
+-/
 
 end LoVe
